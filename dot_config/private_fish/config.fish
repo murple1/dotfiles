2421033e4ed4151bf4844e7b2f406ebb53d205dc
murple1 <murpaza@proton.me>
2026-02-20 23:10:49 -0500

    more files

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,5 +6,5 @@ zoxide init fish | source
 fastfetch
 # Commands to run in interactive sessions can go here
 end
-
+# Shell themeing # Install Starship https://starship.rs/guide/
 starship init fish | source
